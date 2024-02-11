09a1ab9f6fc5efe6eceaa343548694b8656fa349
Jan Bancerewicz <ziomal.kacperox@gmail.com>
2024-02-11 19:38:18 +0100

    update constraint layout + gradle scripts

--- app/src/main/java/com/example/watertracker/MainActivity.java
+++ app/src/main/java/com/example/watertracker/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.watertracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView sum = (TextView) findViewById(R.id.sum);
+        EditText promptcontent = (EditText) findViewById(R.id.promptcontent);
+        Button itemsave = (Button) findViewById(R.id.itemsave);
+        Button reset = (Button) findViewById(R.id.reset);
+
+
+//        TODO db connection
+//         TODO predefined values
+
+
+        itemsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(sum.getText().toString().trim().length() == 0){
+                    sum.setText("0");
+                }
+                int value=Integer.parseInt(sum.getText().toString());
+
+
+                if(promptcontent.getText().toString().trim().length() == 0){
+                    Toast.makeText(MainActivity.this, "Provide a number", Toast.LENGTH_SHORT).show();
+                }else{
+                    value+=Integer.parseInt(promptcontent.getText().toString());
+                    sum.setText(String.valueOf(value));
+                }
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sum.setText("0");
+                promptcontent.setText("");
+
+            }
+        });
+
+
     }
+
 }
